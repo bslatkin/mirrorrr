d52619597720700ed2ed71ae708995fcf63f6fd4
Brett Slatkin <brett@haxor.com>
2014-04-20 18:33:15 -0700

    Fixing copyright

--- transform_content.py
+++ transform_content.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
-# Copyright 2008 Brett Slatkin
-# 
+# Copyright 2008-2014 Brett Slatkin
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -78,7 +78,7 @@ REPLACEMENT_REGEXES = [
 
   (CSS_URL_START + SAME_DIR_URL_REGEX,
      "url(\g<quote>%(accessed_dir)s\g<url>"),
-  
+
   (CSS_URL_START + TRAVERSAL_URL_REGEX,
       "url(\g<quote>%(accessed_dir)s/\g<relative>/\g<url>"),
 
