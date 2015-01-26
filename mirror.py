ff4c1e70c26f7e8573894a51007c82af9af5f719
solsTiCe d'Hiver <solstice.dhiver@gmail.com>
2015-01-26 09:43:45 +0100

    Clean up
    * Remove unused url
    * Fix warmup url
    * Remove base.#.css
    * Remove cleanup.html

--- mirror.py
+++ mirror.py
@@ -236,6 +236,6 @@ class MirrorHandler(BaseHandler):
 app = webapp2.WSGIApplication([
   (r"/", HomeHandler),
   (r"/main", HomeHandler),
+  (r"/_ah/warmup", WarmupHandler),
   (r"/([^/]+).*", MirrorHandler),
-  (r"/warmup", WarmupHandler),
 ], debug=DEBUG)
